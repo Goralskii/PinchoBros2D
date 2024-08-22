de6b7de201feed584281855a2c4859af52214b0c
nicogoral31 <111999252+Goralskii@users.noreply.github.com>
2024-08-22 17:42:08 -0300

    update tilemap

--- PinchoBros2D/Assets/Scripts/MovimientoDelJugador.cs
+++ PinchoBros2D/Assets/Scripts/MovimientoDelJugador.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlDeJugador : MonoBehaviour
+public class MovimientoDelJugador : MonoBehaviour
 {
     public float runSpeed = 2;
 
@@ -27,10 +27,9 @@ public class ControlDeJugador : MonoBehaviour
   
     void Update()
     {
-
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            rb2D.velocity = new Vector2 (runSpeed, rb2D.velocity.y);
+            rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
             animator.SetFloat("movimiento", rb2D.velocity.x);
             animator.SetBool("reposo", false);
             pinchoSprite.flipX = true;
@@ -44,40 +43,25 @@ public class ControlDeJugador : MonoBehaviour
         }
         else
         {
-            rb2D.velocity = new Vector2(0,rb2D.velocity.y);
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
         }
 
         if (_comprobarSuelo.enSuelo)
         {
             animator.SetBool("enSuelo", true);
+
             if (Input.GetKey("w") || Input.GetKey("up"))
             {
                 rb2D.velocity = new Vector2(rb2D.velocity.x, JumpSpeed);
-                animator.SetBool("saltando", true);
-                animator.SetBool("levantarse", true) ;
+                animator.SetBool("enSuelo", false) ;
             }
-        }
 
-        if (rb2D.velocity.x == 0)
-        {
-            animator.SetBool("reposo", true);
-
-            if (Input.GetKey("s") || Input.GetKey("down"))
+            if (rb2D.velocity.x == 0)
             {
-                animator.SetBool("sentarse", true);
+                animator.SetBool("reposo", true);
             }
         }
-        //if(rb2D.velocity.x == 0)
-        //{
-        //    animator.SetBool("reposo", true);
-        //    if (Input.GetKey("s") || Input.GetKey("down"))
-        //    {
-        //        animator.SetBool("sentarse", true) ;
-        //    }
-        //    else if (Input.GetKey("space"))
-        //    {
-        //        animator.SetBool("levantarse", true);
-        //    }
-        //}
+
+
     }
 }
